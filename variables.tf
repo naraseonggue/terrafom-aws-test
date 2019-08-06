d1b4a74e2a6edb4ae010be2d1d479edef2fadbdc
naraseonggue <naraseonggue@gmail.com>
2019-08-06 11:08:25 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,26 +6,21 @@ variable "aws_region" {
 }
 
 variable "vpc_cidr_block" {
-    default     =   "172.16.0.0/16"
+    default     =   "192.168.2.0/25"
 }
 
 variable "vpc_public_subnets" {
     default = [
         {
             "az"    =   "ap-northeast-2a",
-            "cidr"  =   "172.16.1.0/24",
+            "cidr"  =   "192.168.2.0/27",
             "name"  =   "public-subnet-01"
         },
         {
-            "az"    =   "ap-northeast-2b",
-            "cidr"  =   "172.16.2.0/24",
+            "az"    =   "ap-northeast-2c",
+            "cidr"  =   "192.168.2.32/27",
             "name"  =   "public-subnet-02"
         },
-        {
-            "az"    =   "ap-northeast-2c",
-            "cidr"  =   "172.16.3.0/24",
-            "name"  =   "public-subnet-03"
-        }
     ]
     description     =   "Subnet list to use for network to connect to the internet in this VPC"
     type            =   "list"
@@ -35,19 +30,14 @@ variable "vpc_private_subnets" {
     default = [
         {
             "az"    =   "ap-northeast-2a",
-            "cidr"  =   "172.16.16.0/24",
+            "cidr"  =   "192.168.2.64/27",
             "name"  =   "private-subnet-01"
         },
         {
-            "az"    =   "ap-northeast-2b",
-            "cidr"  =   "172.16.17.0/24",
+            "az"    =   "ap-northeast-2c",
+            "cidr"  =   "192.168.2.96/27",
             "name"  =   "private-subnet-02"
         },
-        {
-            "az"    =   "ap-northeast-2c",
-            "cidr"  =   "172.16.18.0/24",
-            "name"  =   "private-subnet-03"
-        }
     ]
     description     =   "CIDR Blocks of subnet to use for internal network in this VPC"
     type            =   "list"
