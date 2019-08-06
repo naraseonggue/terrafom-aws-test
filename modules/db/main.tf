87e4e983f071ed3da6eaf7eee38a07e75f28cb57
naraseonggue <naraseonggue@gmail.com>
2019-08-06 17:07:12 +0900

    Update main.tf

--- modules/db/main.tf
+++ modules/db/main.tf
@@ -1,1 +1,16 @@
+resource "aws_security_group" "rds" {
+  name = "${format("%s-rds-sg", var.name)}"
 
+  vpc_id = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port   = "${var.db_port}"
+    to_port     = "${var.db_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["${module.vpc.private_subnets_cidr_blocks}"]
+  }
+
+  tags {
+    Group = "${var.name}"
+  }
+}
